bfbfdfa1c97dae0f297cfd5b396016418833c426
Michael Comella <michael.l.comella@gmail.com>
2016-02-18 22:03:28 -0800

    Get app (sort of) loaded into browser.
    It loads and calls out to the mozhg functions, but is not connected to
    the react components.
    
    Note also that the app doesn't run in Firefox due to syntax error
    in jsdom - I'll probably need to switch from using it, also because
    it's quite large.

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
-var express = require('express'),
-    mozhg = require('./lib/mozhg');
-var	app = express();
+var mozhg = require('./lib/mozhg');
 
-app.get('/changeset/:changesetId', (req, res) => {
-    // TODO: validate changesetid.
-    var changesetToVerifyId = req.params.changesetId;
+// TODO: Validate changeset.
+var changesetToVerifyId = '7ced13d644be';
 
-    mozhg.getLatestNightlyChangesetId((err, latestNightlyChangesetId) => {
+console.log('running application');
+
+mozhg.getLatestNightlyChangesetId((err, latestNightlyChangesetId) => {
+    if (err) {
+        // TODO: Pass errors to client.
+        console.error(err);
+        return;
+    }
+    // TODO: It's slow if the answer is false.
+    mozhg.isRevisionSetValid(changesetToVerifyId, latestNightlyChangesetId, (err, valid) => {
         if (err) {
             // TODO: Pass errors to client.
             console.error(err);
             return;
         }
-
-        // TODO: It's slow if the answer is false.
-        mozhg.isRevisionSetValid(changesetToVerifyId, latestNightlyChangesetId, (err, valid) => {
-            if (err) {
-                // TODO: Pass errors to client.
-                console.error(err);
-                return;
-            }
-
-            console.log('%s is in Nightly: %s', changesetToVerifyId, valid);
-            res.send({isOnNightly: valid});
-        });
+        console.log('%s is in Nightly: %s', changesetToVerifyId, valid);
     });
-});
-
-app.use(express.static('public'));
-
-var port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log('Listening on port ' + port);
 });
