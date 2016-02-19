b416577ace851e2c8d464d4eeca4d5acf3b9dfd4
Michael Comella <michael.l.comella@gmail.com>
2016-02-19 07:55:57 -0800

    Hook up React components to mozhg functionality.
    Everything now works in a static page!
    
    ...except it doesn't run in Firefox. :(

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ var ChangesetForm = require('./lib/ui/changesetform').ChangesetForm,
     ReactDOM = require('react-dom');
 
 // TODO: Validate changeset.
-var changesetToVerifyId = '7ced13d644be';
-
-console.log('running application');
-
-mozhg.getLatestNightlyChangesetId((err, latestNightlyChangesetId) => {
-    if (err) {
-        // TODO: Pass errors to client.
-        console.error(err);
-        return;
-    }
-    // TODO: It's slow if the answer is false.
-    mozhg.isRevisionSetValid(changesetToVerifyId, latestNightlyChangesetId, (err, valid) => {
+var isItOnNightly = function (changesetToVerifyId, callback) {
+    mozhg.getLatestNightlyChangesetId((err, latestNightlyChangesetId) => {
         if (err) {
             // TODO: Pass errors to client.
             console.error(err);
             return;
         }
-        console.log('%s is in Nightly: %s', changesetToVerifyId, valid);
+
+        // TODO: It's slow if the answer is false.
+        mozhg.isRevisionSetValid(changesetToVerifyId, latestNightlyChangesetId, (err, valid) => {
+            if (err) {
+                // TODO: Pass errors to client.
+                console.error(err);
+                return;
+            }
+            console.log('%s is in Nightly: %s', changesetToVerifyId, valid);
+            callback(valid);
+        });
     });
-});
+}
 
 ReactDOM.render(
-    <ChangesetForm url='changeset/' />,
+    <ChangesetForm callback={isItOnNightly} />,
     document.getElementById('form_content')
 );
